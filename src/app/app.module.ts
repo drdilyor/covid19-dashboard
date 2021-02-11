743d2dd61c3f8d7c4fb63028a12320dc2d04e7eb
drdilyor <drdilyor@outlook.com>
2021-02-11 18:32:31 +0500

    3 Split app to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { MainStatsComponent } from './main-stats/main-stats.component';
+import { CountryStatsComponent } from './country-stats/country-stats.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainStatsComponent,
+    CountryStatsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
