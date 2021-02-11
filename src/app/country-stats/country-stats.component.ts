d966509c5c954befbbbaa2373a07ca4c5e947545
drdilyor <drdilyor@outlook.com>
2021-02-11 19:45:51 +0500

    4 country stats now uses api

--- src/app/country-stats/country-stats.component.ts
+++ src/app/country-stats/country-stats.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-country-stats',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./country-stats.component.scss']
 })
 export class CountryStatsComponent implements OnInit {
+  countryStats: any;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.httpClient.get('https://disease.sh/v3/covid-19/countries').subscribe((data: any) => {
+      this.countryStats = data;
+    });
   }
 
 }
